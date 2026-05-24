4c2ba2cc50cb64f943dc56577986385babe28d04
MauricioDeveloperBR <ferreira31_mauricio@protonmail.com>
2026-05-23 21:20:55 -0300

    feat: Adiciona classe conversor celsius-Fahrenheit

--- src/app_scripts/app.js
+++ src/app_scripts/app.js
@@ -7,7 +7,7 @@ class ConvesorTemperatura {
 }
 
 
-class CelsiusToFahrenheitConverter extends ConvesorTemperatura {
+class CelsiusEmFahrenheit extends ConvesorTemperatura {
     converte(value) {
         return {
             value: Math.round(value * 1.8 + 32),
